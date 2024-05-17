d39b45994493868070084ea94ddb157930b5c8d1
platon-bo <platon.bolsheshapov@gmail.com>
2024-05-18 04:10:27 +0700

    project loaded to git

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const users = require("../models/user");
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  users
+    .findUserByCredentials(email, password)
+    .then((user) => {
+      res
+        .status(200)
+        .send({ _id: user._id, username: user.username, email: user.email });
+    })
+    .catch((error) => {
+      res.status(401).send({ message: error.message });
+    });
+};
+
+module.exports = { login };
